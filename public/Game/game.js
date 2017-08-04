fd6297758207dd8ba09f8ffc7a8fff7f2ec6d65c
freddydumont <frederickmorin18@gmail.com>
2017-08-04 14:38:55 -0400

    Clean up code to use Game namespace
    * Implement Game namespace and init function
    * Generate map
    * Implement freecells array
    * Add player

--- public/Game/game.js
+++ public/Game/game.js
@@ -1,52 +1,84 @@
-// Check if rot.js can work on this browser
-if (!ROT.isSupported()) {
-    alert("The rot.js library isn't supported by your browser.");
-} else {
-    // width and height
-    ROT.DEFAULT_HEIGHT = 25;
-    ROT.DEFAULT_WIDTH = 36;
+// width and height
+ROT.DEFAULT_HEIGHT = 25;
+ROT.DEFAULT_WIDTH = 36;
 
-    // Create display, declare and generate map type
-    let display = new ROT.Display();
-    let container = display.getContainer();
-    let map = new ROT.Map.Arena();
+let Game = {
+    display: null,
+
+    init: function () {
+        // create display with rot defaults
+        this.display = new ROT.Display();
+
+        // Add the container to our HTML page
+        document.getElementsByClassName("game-container")[0].appendChild(this.display.getContainer());
+        // call map generation function
+        this.generateMap();
+    },
+};
+
+// declare map
+Game.map = {};
+Game.generateMap = function () {
+    // generate map type
+    let arena = new ROT.Map.Arena();
 
     // store empty cells as array of arrays [[x1,y1],[x2,y2],...]
     let freeCells = [];
-    let player = null;
 
-    // Add the container to our HTML page
-    document.getElementsByClassName("game-container")[0].appendChild(container);
-
-    // Draw map on display
-    let Player = function (x, y) {
-        this._x = x;
-        this._y = y;
-        this.draw();
+    // create map
+    mapCallback = function (x, y, wall) {
+        if (!wall) {
+            // store empty cells as array of arrays
+            freeCells.push([x, y]);
+        }
+        this.display.draw(x, y, wall ? "#" : ".")
     }
-    Player.prototype.draw = function () {
-        display.draw(this._x, this._y, "@", "#ff0")
-    }
-    let createPlayer = function (freeCells) {
-        // random a position for Player to spawn in
-        let index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-        var key = freeCells.splice(index, 1)[0];
-        let x = key[0];
-        let y = key[1];
-        this.player = new Player(x, y);
-    }
+    arena.create(mapCallback.bind(this));
 
-    let generateMap = function () {
-        map.create(function (x, y, wall) {
-            if (!wall) {
-                // store empty cells as array of arrays
-                freeCells.push([x, y]);
-            }
-            display.draw(x, y, wall ? "#" : ".")
-        });
-        createPlayer(freeCells)
+    // call draw function
+    this.drawWholeMap();
+
+    // call function to display player on a free cell
+    this.createPlayer(freeCells);
+}
+
+// iterate through all the floor tiles and draw their visual representation
+Game.drawWholeMap = function () {
+    for (let key in this.map) {
+        let parts = key.split(",");
+        let x = parseInt(parts[0]);
+        let y = parseInt(parts[1]);
+        this.display.draw(x, y, this.map[key]);
     }
-    generateMap();
+}
 
+// Initialize Player
+Game.player = null;
+let Player = function (x, y) {
+    this._x = x;
+    this._y = y;
+    this.draw();
+}
 
+Player.prototype.draw = function () {
+    Game.display.draw(this._x, this._y, "@", "#ff0")
+}
+
+Game.createPlayer = function (freeCells) {
+    // random a position for Player to spawn in
+    let index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+    var key = freeCells.splice(index, 1)[0];
+    let x = key[0];
+    let y = key[1];
+    this.player = new Player(x, y);
+}
+
+window.onload = function () {
+    // Check if rot.js can work on this browser
+    if (!ROT.isSupported()) {
+        alert("The rot.js library isn't supported by your browser.");
+    } else {
+        // Initialize the game
+        Game.init();
+    }
 }
