4510c1c24ae33649b39047061e573a31b11fc782
freddydumont <frederickmorin18@gmail.com>
2017-08-22 14:34:02 -0400

    Add messages.js for message related functions
    * Add sendMessage function

---
+++ src/Game/messages.js
@@ -0,0 +1,18 @@
+import { vsprintf } from 'sprintf-js';
+import Mixins from './mixins';
+
+/**
+ * Take at least a recipient entity and a message as arguments,
+ * as well as an optional array of parameters to pass to vsprintf
+ * which is a function that accepts it's formatting arguments as an array.
+ */
+export const sendMessage = function(recipient, message, args) {
+  // Make sure the recipient can receive the message before doing any work.
+  if (recipient.hasMixin(Mixins.MessageRecipient)) {
+    // If args were passed, then we format the message, else no formatting is necessary
+    if (args) {
+      message = vsprintf(message, args);
+    }
+    recipient.receiveMessage(message);
+  }
+};
