3ba1bb4fa81568997b3196e24a3a6c20583b6424
freddydumont <frederickmorin18@gmail.com>
2017-08-22 14:48:41 -0400

    Add sendMessageNearby function

--- src/Game/messages.js
+++ src/Game/messages.js
@@ -17,3 +17,28 @@ export const sendMessage = function(recipient, message, args) {
     recipient.receiveMessage(message);
   }
 };
+
+/**
+ * This function specifies a location rather than a recipient entity.
+ * Sends the message to all entities within a given radius from the location.
+ */
+export const sendMessageNearby = function(
+  map,
+  centerX,
+  centerY,
+  message,
+  args
+) {
+  // If args were passed, then we format the message, else no formatting is necessary
+  if (args) {
+    message = vsprintf(message, args);
+  }
+  // Get the nearby entities
+  let entities = map.getEntitiesWithinRadius(centerX, centerY, 5);
+  // Iterate through nearby entities, sending the message if they can receive it.
+  for (let i = 0; i < entities.length; i++) {
+    if (entities[i].hasMixin(Mixins.MessageRecipient)) {
+      entities[i].receiveMessage(message);
+    }
+  }
+};
