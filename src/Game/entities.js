f768080868d78fd89eb30d5cf3eb1a14799955e3
Clarence Chan <programclarence@gmail.com>
2017-08-23 20:53:34 -0400

    Changed entities object into entities repo object.

--- src/Game/entities.js
+++ src/Game/entities.js
@@ -1,4 +1,6 @@
 import Mixins from './mixins';
+import Repository from './repository';
+import Entity from './entity';
 
 const Entities = {
   Player: {
@@ -15,35 +17,37 @@ const Entities = {
       Mixins.Sight,
       Mixins.MessageRecipient
     ]
-  },
-
-  Bat: {
-    name: 'bat',
-    character: 'B',
-    foreground: 'white',
-    maxHp: 5,
-    attackValue: 4,
-    mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
-  },
-
-  Newt: {
-    name: 'newt',
-    character: ':',
-    foreground: 'yellow',
-    maxHp: 3,
-    attackValue: 2,
-    mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
-  },
-
-  ToughGuy: {
-    name: 'tough guy',
-    character: 'E',
-    foreground: 'red',
-    maxHp: 15,
-    defenseValue: 5,
-    attackValue: 3,
-    mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
   }
 };
+
+export const EntityRepository = new Repository('entities', Entity);
+
+EntityRepository.define('tough guy', {
+  name: 'tough guy',
+  character: 'E',
+  foreground: 'red',
+  maxHp: 15,
+  defenseValue: 5,
+  attackValue: 3,
+  mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
+});
+
+EntityRepository.define('bat', {
+  name: 'bat',
+  character: 'B',
+  foreground: 'white',
+  maxHp: 5,
+  attackValue: 4,
+  mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
+});
+
+EntityRepository.define('newt', {
+  name: 'newt',
+  character: ':',
+  foreground: 'yellow',
+  maxHp: 3,
+  attackValue: 2,
+  mixins: [Mixins.WanderActor, Mixins.Attacker, Mixins.Destructible]
+});
 
 export default Entities;
