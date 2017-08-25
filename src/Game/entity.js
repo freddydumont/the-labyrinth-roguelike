7552f9b51cf1024d34dabe84d49325ef6f151be7
Clarence Chan <programclarence@gmail.com>
2017-08-25 01:31:50 -0400

    Added class DynamicGlyph. Changed class Entity to extend off class DynamicGlyph.

--- src/Game/entity.js
+++ src/Game/entity.js
@@ -1,4 +1,4 @@
-import Glyph from './glyph';
+import DynamicGlyph from './dynamicglyph';
 import Tile from './tile';
 import Mixins from './mixins';
 import * as Messages from './messages';
@@ -7,57 +7,14 @@ import * as Messages from './messages';
  * A basic entity is composed of a glyph as well as a position and a name (used in messages).
  * An entity can be anything. Ex. Player, enemy, items, etc
  */
-export default class Entity extends Glyph {
+export default class Entity extends DynamicGlyph {
   constructor(props) {
     super(props);
     // Instantiate any properties from the passed object
-    this._name = props['name'] || '';
     this._x = props['x'] || 0;
     this._y = props['y'] || 0;
     this._z = props['z'] || 0;
     this._map = null;
-    // Create an object which will keep track what mixins we have
-    // attached to this entity based on the name property
-    this._attachedMixins = {};
-    // Create a similar object for groups
-    this._attachedMixinGroups = {};
-    // Setup the object's mixins
-    this.setupMixins(props);
-  }
-
-  // Mixin functions
-  setupMixins(props) {
-    let mixins = props['mixins'] || [];
-    for (let i = 0; i < mixins.length; i++) {
-      // Copy over all properties from each mixin as long
-      // as it's not the name or the init property. We
-      // also make sure not to override a property that
-      // already exists on the entity.
-      for (let key in mixins[i]) {
-        if (key !== 'init' && key !== 'name' && !this.hasOwnProperty(key)) {
-          this[key] = mixins[i][key];
-        }
-      }
-      // Add the name of this mixin to our attached mixins
-      this._attachedMixins[mixins[i].name] = true;
-      // If a group name is present, add it
-      if (mixins[i].groupName) {
-        this._attachedMixinGroups[mixins[i].groupName] = true;
-      }
-      // Finally call the init function if there is one
-      if (mixins[i].init) {
-        mixins[i].init.call(this, props);
-      }
-    }
-  }
-
-  hasMixin(obj) {
-    // Allow passing the mixin itself or the name as a string
-    if (typeof obj === 'object') {
-      return this._attachedMixins[obj.name];
-    } else {
-      return this._attachedMixins[obj] || this._attachedMixinGroups[obj];
-    }
   }
 
   setPosition(x, y, z) {
@@ -137,9 +94,6 @@ export default class Entity extends Glyph {
   }
 
   // setters
-  setName(name) {
-    this._name = name;
-  }
   setX(x) {
     this._x = x;
   }
@@ -154,9 +108,6 @@ export default class Entity extends Glyph {
   }
 
   // getters
-  getName() {
-    return this._name;
-  }
   getX() {
     return this._x;
   }
