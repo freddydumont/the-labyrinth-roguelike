1c045afc414a64cc35b6a89855a64554c2d7a1d8
freddydumont <frederickmorin18@gmail.com>
2017-08-26 19:03:13 -0400

    Add equippable item mixin

--- src/Game/itemmixins.js
+++ src/Game/itemmixins.js
@@ -1,4 +1,5 @@
 import Item from './item';
+
 const ItemMixins = {
   // Edible mixins
   Edible: {
@@ -27,6 +28,32 @@ const ItemMixins = {
       } else {
         return this._name;
       }
+    }
+  },
+
+  Equippable: {
+    name: 'Equippable',
+    init: function(template) {
+      this._attackValue = template['attackValue'] || 0;
+      this._defenseValue = template['defenseValue'] || 0;
+      this._wieldable = template['wieldable'] || false;
+      this._wearable = template['wearable'] || false;
+    },
+
+    getAttackValue: function() {
+      return this._attackValue;
+    },
+
+    getDefenseValue: function() {
+      return this._defenseValue;
+    },
+
+    isWieldable: function() {
+      return this._wieldable;
+    },
+
+    isWearable: function() {
+      return this._wearable;
     }
   }
 };
