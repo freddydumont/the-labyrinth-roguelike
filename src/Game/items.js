191a5808bcf3654aa3eb9dca15fb67eeaa8421be
freddydumont <frederickmorin18@gmail.com>
2017-08-26 19:09:31 -0400

    Add basic items

--- src/Game/items.js
+++ src/Game/items.js
@@ -4,11 +4,7 @@ import ItemMixins from './itemmixins';
 
 export const ItemRepository = new Repository('items', Item);
 
-ItemRepository.define('rock', {
-  name: 'rock',
-  character: '*',
-  foreground: 'white'
-});
+// Edibles
 ItemRepository.define('apple', {
   name: 'apple',
   character: '%',
@@ -16,6 +12,7 @@ ItemRepository.define('apple', {
   foodValue: 50,
   mixins: [ItemMixins.Edible]
 });
+
 ItemRepository.define('melon', {
   name: 'melon',
   character: '%',
@@ -24,6 +21,7 @@ ItemRepository.define('melon', {
   consumptions: 4,
   mixins: [ItemMixins.Edible]
 });
+
 ItemRepository.define(
   'corpse',
   {
@@ -37,3 +35,103 @@ ItemRepository.define(
     disableRandomCreation: true
   }
 );
+
+// Weapons
+ItemRepository.define(
+  'dagger',
+  {
+    name: 'dagger',
+    character: ')',
+    foreground: 'gray',
+    attackValue: 5,
+    wieldable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+ItemRepository.define(
+  'sword',
+  {
+    name: 'sword',
+    character: ')',
+    foreground: 'white',
+    attackValue: 10,
+    wieldable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+ItemRepository.define(
+  'staff',
+  {
+    name: 'staff',
+    character: ')',
+    foreground: 'yellow',
+    attackValue: 5,
+    defenseValue: 3,
+    wieldable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+// Wearables
+ItemRepository.define(
+  'tunic',
+  {
+    name: 'tunic',
+    character: '[',
+    foreground: 'green',
+    defenseValue: 2,
+    wearable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+ItemRepository.define(
+  'chainmail',
+  {
+    name: 'chainmail',
+    character: '[',
+    foreground: 'white',
+    defenseValue: 4,
+    wearable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+ItemRepository.define(
+  'platemail',
+  {
+    name: 'platemail',
+    character: '[',
+    foreground: 'aliceblue',
+    defenseValue: 6,
+    wearable: true,
+    mixins: [ItemMixins.Equippable]
+  },
+  {
+    disableRandomCreation: true
+  }
+);
+
+// Other
+ItemRepository.define('rock', {
+  name: 'rock',
+  character: '*',
+  foreground: 'white'
+});
