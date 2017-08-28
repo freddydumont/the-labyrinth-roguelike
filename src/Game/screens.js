fae7daaab65e6230cf863feefc4be4d3c53a0b49
Clarence Chan <programclarence@gmail.com>
2017-08-28 17:16:15 -0400

    Added examine screen allow players to examine items in their inventory.

--- src/Game/screens.js
+++ src/Game/screens.js
@@ -600,5 +600,24 @@ Screen.wearScreen = new ItemListScreen({
     return true;
   }
 });
+Screen.examineScreen = new Game.Screen.ItemListScreen({
+  caption: 'Choose the item you wish to examine',
+  canSelect: true,
+  canSelectMultipleItems: false,
+  isAcceptable: function(item) {
+    return true;
+  },
+  ok: function(selectedItems) {
+    const keys = Object.keys(selectedItems);
+    if (keys.length > 0) {
+      let item = selectedItems[keys[0]];
+      Messages.sendMessage(this._player, "It's %s (%s).", [
+        item.describeA(false),
+        item.details()
+      ]);
+    }
+    return true;
+  }
+});
 
 export default Screen;
