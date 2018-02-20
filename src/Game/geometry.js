6f64c9c7dd1e49ff5344260ca2c50e77a751ae38
Unknown <frederickmorin18@gmail.com>
2018-02-20 21:10:58 +1300

    Add getCardinal function to geometry.js
    getCardinal returns the relative cardinal direction of two coordinates

--- src/Game/geometry.js
+++ src/Game/geometry.js
@@ -28,7 +28,26 @@ const Geometry = {
     }
 
     return points;
-  }
+  },
+
+  getCardinal: function(x1, y1, x2, y2) {
+    // https://stackoverflow.com/questions/35104991/
+    // relative-cardinal-direction-of-two-coordinates
+    const directions = {
+      6: 'North',
+      7: 'Northwest',
+      0: 'West',
+      1: 'Southwest',
+      2: 'South',
+      3: 'Southeast',
+      4: 'East',
+      5: 'Northeast',
+    };
+
+    let angle = (Math.atan2(y2 - y1, x2 - x1) + Math.PI) / (Math.PI / 4);
+    // round angle for more precision
+    return directions[Math.round(angle) % 8];
+  },
 };
 
 export default Geometry;
